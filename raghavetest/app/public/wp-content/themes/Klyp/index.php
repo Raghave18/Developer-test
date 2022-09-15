98cc8af1843a6bb2f2a96582c14caf45ed3d26a0
Raghave Gupta <raghave18@gmail.com>
2022-09-15 20:37:56 +1000

    Table added

--- raghavetest/app/public/wp-content/themes/Klyp/index.php
+++ raghavetest/app/public/wp-content/themes/Klyp/index.php
@@ -15,3 +15,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
+<body>
+    <div class="container">
+        <h2 class="text-center mt-3 mb-3"><a href="/">Movie Search</a></h2>
+        <form action="index.php" method="get">
+            <input type="hidden" name="action" value="search">
+            <div class="row">
+                <div class="col-10">
+                    <input type="text" class="form-control" placeholder="Movie Name" name="keyword" id="color" value="<?php echo $_GET['keyword']?>">
+                </div>
+                <div class="col-2">
+                    <input type="submit" class="btn btn-info" value="Search">
+                </div>
+            </div>
+        </form>
+        <hr>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Year</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Runtime</th>
+              <th scope="col">imdb Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td><?php echo ($movie_data['Title']) ?></td>
+                <td><?php echo ($movie_data['Year']) ?></td>
+                <td><?php echo ($movie_data['Genre']) ?></td>
+                <td><?php echo ($movie_data['Runtime']) ?></td>
+                <td><?php echo ($movie_data['imdbRating']) ?></td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+</body>
+</html>
